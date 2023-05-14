2928d8fdbc2c3ee30d7dcd64920b4648ccaa5399
Brain <133573012+ShersheLeWidow@users.noreply.github.com>
2023-05-14 13:46:24 +0200

    Create matrix.py

---
+++ matrix.py
@@ -0,0 +1,41 @@
+def add_matrices(matrix1, matrix2):
+    result = []
+    for i in range(len(matrix1)):
+        row = []
+        for j in range(len(matrix1[0])):
+            row.append(matrix1[i][j] + matrix2[i][j])
+        result.append(row)
+    return result
+
+# function to subtract two matrices
+def subtract_matrices(matrix1, matrix2):
+    result = []
+    for i in range(len(matrix1)):
+        row = []
+        for j in range(len(matrix1[0])):
+            row.append(matrix1[i][j] - matrix2[i][j])
+        result.append(row)
+    return result
+
+# function to multiply two matrices
+def multiply_matrices(matrix1, matrix2):
+    result = []
+    for i in range(len(matrix1)):
+        row = []
+        for j in range(len(matrix2[0])):
+            sum = 0
+            for k in range(len(matrix2)):
+                sum += matrix1[i][k] * matrix2[k][j]
+            row.append(sum)
+        result.append(row)
+    return result
+
+# function to transpose a matrix
+def transpose_matrix(matrix):
+    result = []
+    for i in range(len(matrix[0])):
+        row = []
+        for j in range(len(matrix)):
+            row.append(matrix[j][i])
+        result.append(row)
+    return result
