8e5172c48816a3dbc49dbdcacb007654984883ca
Brain <133573012+ShersheLeWidow@users.noreply.github.com>
2023-05-19 19:52:12 +0200

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,7 +1,7 @@
 def add_matrices(matrix1, matrix2):
     result = []
     for i in range(len(matrix1)):
-        row = []
+#sst        row = []
         for j in range(len(matrix1[0])):
             row.append(matrix1[i][j] + matrix2[i][j])
         result.append(row)
